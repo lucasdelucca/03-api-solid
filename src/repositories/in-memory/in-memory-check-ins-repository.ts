e97f70634a297f8c3a6f77bfb5f991e04f1d1af2
Lucas de Lucca <llucca54@gmail.com>
2024-05-22 11:06:51 -0300

    feat: implement prisma check-in and gyms repositories

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,4 +1,4 @@
-import { CheckIn, CheckIn, Prisma } from '@prisma/client'
+import { CheckIn, Prisma } from '@prisma/client'
 import { CheckInsRepository } from '../check-ins-repository'
 import { randomUUID } from 'node:crypto'
 import dayjs from 'dayjs'
