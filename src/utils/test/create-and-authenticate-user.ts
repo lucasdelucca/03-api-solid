822ddc9cc19b1b8670a97a96426a84e04eef4cf0
Lucas de Lucca <llucca54@gmail.com>
2024-05-24 16:20:29 -0300

    ci: add e2e tests workflow

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -2,6 +2,7 @@ import { prisma } from '@/lib/prisma'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 import bcrypt from 'bcryptjs'
+import { randomUUID } from 'crypto'
 
 interface CreateAndAuthenticateUserDTO {
   app: FastifyInstance
@@ -10,15 +11,15 @@ interface CreateAndAuthenticateUserDTO {
 
 export async function createAndAuthenticateUser({
   app,
-  role,
 }: CreateAndAuthenticateUserDTO) {
   await prisma.user.create({
     data: {
+      id: randomUUID(),
       name: 'John Doe',
       email: 'johndoe@example.com',
       password_hash: await bcrypt.hash('123456', 6),
       created_at: new Date(),
-      role: role ?? 'MEMBER',
+      role: 'ADMIN',
     },
   })
 
